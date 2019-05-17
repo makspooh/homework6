67a85644894b5d9fcb1301c16899cb91f2afd462
Maksim Pokhiliy <maksimpooh@gmail.com>
2019-05-17 09:15:06 +0300

    Update classWork6.js

--- classWork6.js
+++ classWork6.js
@@ -1,16 +1,22 @@
 'use strict';
 
+// task 1
+// 1.1 В ответе есть заголовок "access-control-allow-origin: *"
+// 1.2 В ответе нет заголовков "response-headers"
+// 1.3 В ответе есть заголовок "access-control-allow-origin: https://facebool.com"
+
+// task 2
 function sentRequest(url) {
   return new Promise(function(resolve, reject) {
 
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     
     xhr.onload = function() {
       if (this.status == 200) {
         resolve(this.response);
       } else {
-        let error = new Error(this.statusText);
+        const error = new Error(this.statusText);
         error.code = this.status;
         reject(error);
       }
@@ -25,20 +31,18 @@ function sentRequest(url) {
 }
 sentRequest('https://tanuhaua.github.io/datas-file-json/github_users.json')
   .then(response => {
-    let users = JSON.parse(response);
-    return users;
+    return JSON.parse(response);
   })
   .then(users => {
     users.forEach(user => {
-      return sentRequest(`https://api.github.com/users/${user.githubName}`)
+      sentRequest(`https://api.github.com/users/${user.githubName}`)
         .then(response => {
-          let user = JSON.parse(response);
-          let body = document.querySelector('body');
-          let block = document.createElement('div');
-          let login = user.login;
-          let paragraph = document.createElement('p');
-          let img = document.createElement('img');
-          let src = user.avatar_url;
+          const user = JSON.parse(response);
+          const body = document.querySelector('body');
+          const block = document.createElement('div');
+          const [login, src] = [user.login, user.avatar_url];
+          const paragraph = document.createElement('p');
+          const img = document.createElement('img');
 
           paragraph.textContent = login;
           img.setAttribute('src', src);
@@ -59,26 +63,24 @@ sentRequest('https://tanuhaua.github.io/datas-file-json/github_users.json')
 
           img.style.width = '250px';
           img.style.borderRadius = '50%';
-        })
-    })
+        });
+    });
   })
 
+// task 3
 function sentFetchRequest(url) {
   fetch(url)  
     .then(response => response.json())
     .then(data => {
-      let users = data;
-      users.forEach(user => {
-        fetch(`https://api.github.com/users/${user.githubName}`)
+      data.forEach(users => {
+        fetch(`https://api.github.com/users/${users.githubName}`)
           .then(response => response.json())
           .then(data => {
-            let users = data;
-            let body = document.querySelector('body');
-            let block = document.createElement('div');
-            let login = users.login;
-            let paragraph = document.createElement('p');
-            let img = document.createElement('img');
-            let src = users.avatar_url;
+            const body = document.querySelector('body');
+            const block = document.createElement('div');
+            const [login, src] = [data.login, data.avatar_url];
+            const paragraph = document.createElement('p');
+            const img = document.createElement('img');
     
             paragraph.textContent = login;
             img.setAttribute('src', src);
@@ -99,7 +101,7 @@ function sentFetchRequest(url) {
     
             img.style.width = '250px';
             img.style.borderRadius = '50%';
-          })
+          });
       });
     })
   }
